9dc09e655ffccba64282b8ce0a98b19b6d557b12
Lucca Dias <luccadiass@gmail.com>
2016-11-16 16:06:25 -0200

    abrir box

---
+++ js/box.js
@@ -0,0 +1,7 @@
+/*$(document).ready(function(){
+    var maker='.boxBAR';
+    $('.boxBAR').css('display','none')
+    $('.boxBAR').click(function(){
+        $('.boxBAR').css('display','block')
+    })
+});
